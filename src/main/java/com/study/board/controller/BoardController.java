84b6f09b4f38b03c4fe2986cf6efd84a26c5aab6
Jeong, Kyue-won <jkw__@users.noreply.github.com>
2023-01-13 14:55:24 +0900

    commit #5

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,23 +1,15 @@
-package com.study.board.Controller;
+package com.study.board.controller;
 
 import com.study.board.entity.Board;
 import com.study.board.serviece.BoardService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
-    /*
-    *test String
-    @GetMapping("/")
-    @ResponseBody
-    public String main(){
-        return "Spring Boot Start!";
-    }
-     */
     @Autowired
     private BoardService boardService;
     @GetMapping("/board/write") //board/write로 들어오면
@@ -25,8 +17,16 @@ public class BoardController {
         return "boardwrite";    //resources/templates에 있는 boardwrite.html 페이지를 보여주겠다
     }
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board){
+    public String boardWritePro(@NotNull Board board){
+        System.out.println("id: "+board.getId());
+        System.out.println("title: "+board.getTitle());
+        System.out.println("content: "+board.getContent());
         boardService.write(board);
         return "";
+    }
+
+    @PostMapping("/board/list")
+    public String boardList(){
+        return "boardlist";
     }
 }
--- src/main/java/com/study/board/entity/Board.java
+++ src/main/java/com/study/board/entity/Board.java
@@ -1,18 +1,13 @@
-/*
- * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
 package com.study.board.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+
 @Entity //entity는 db에서 table을 의미한다
+@Data
 public class Board { //JPA가 이 엔티티를 읽는다
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // identity는 mysql, mariadb sequence는 oracle auto는 알아서 지정
