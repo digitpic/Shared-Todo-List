4ea30a95fb9fb23d02a7bd9f5bda94842f85f580
Kyuwon Jeong <32204027@dankook.ac.kr>
2023-01-14 17:22:41 +0900

    React 추가

--- src/main/java/com/study/board/entity/Board.java
+++ src/main/java/com/study/board/entity/Board.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity //entity는 db에서 table을 의미한다
-@Data
+@Data   //지양해야 할 표현, getter setter toString 전부 만들어주는 annotation
 public class Board { //JPA가 이 엔티티를 읽는다
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // identity는 mysql, mariadb sequence는 oracle auto는 알아서 지정
